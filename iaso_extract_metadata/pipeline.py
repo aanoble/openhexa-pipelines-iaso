a3b38b76abbdfca16fa1b98f63c30baea004c1b2
arnaudanoble17 <aanoble@bluesquarehub.com>
2025-06-18 12:12:22 +0000

    feat: test iaso widget

--- iaso_extract_metadata/pipeline.py
+++ iaso_extract_metadata/pipeline.py
@@ -15,12 +15,20 @@ from openhexa.sdk import (
     pipeline,
     workspace,
 )
+from openhexa.sdk.pipelines.parameter import IASOWidget
 from openhexa.toolbox.iaso import IASO, dataframe
 
 
 @pipeline("iaso_extract_metadata")
 @parameter("iaso_connection", name="IASO connection", type=IASOConnection, required=True)
-@parameter("form_id", name="Form ID", type=int, required=True)
+@parameter(
+    "form_id",
+    name="Form ID",
+    type=int,
+    widget=IASOWidget.IASO_FORMS,
+    connection="iaso_connection",
+    required=True,
+)
 @parameter(
     code="output_file_name",
     type=str,
